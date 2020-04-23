7737cae2a8a1e14075a4bd41f24cbb1c5dc2ca93
mark <you@example.com>
2020-04-23 14:51:09 +0300

    up to date

--- project3/objdb.h
+++ project3/objdb.h
@@ -5,9 +5,12 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]);
+void fexit (FILE **fp,const char func[],const int line);
 int open(FILE **fp, char dbname[]);
 int metadata(FILE **fp);
-int find_name (FILE **fp, char objname[]);
+int fend (FILE **fp);
+int **find (FILE **fp, char name[]);
+int find_name (FILE **fp, char objname[], int option);
 int move_block (FILE **fp, FILE **op, char objname[]);
 int import (FILE **fp, char fname[], char objname[]);
 int close(FILE **fp);
