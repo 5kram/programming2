417b7408d9341f31568f69900a7b9aaf6295376f
mark <you@example.com>
2020-04-24 20:21:00 +0300

    Change find type + append struct of names

--- project3/objdb.h
+++ project3/objdb.h
@@ -4,21 +4,22 @@
 #define _OBJDB_H
 #include <stdio.h>
 
-int main(int argc, char *argv[]);
-void fexit(FILE *fp,const char func[],const int line);
-int open(FILE **fp, char dbname[]);
-int metadata(FILE *fp);
-int fend(FILE *fp);
-int **find(FILE *fp, char name[]);
-int find_name(FILE *fp, char objname[], int option);
-int move_block(FILE *fp, FILE *op, char objname[]);
-int import(FILE *fp, char fname[], char objname[]);
-int close(FILE *fp);
-
 typedef struct {
     unsigned int num_results;
     unsigned int *names;
     char *names_buffer;
 } FindResult;
+
+int main(int argc, char *argv[]);
+void fexit(FILE *fp,const char func[],const int line);
+int open(FILE **fp, char dbname[]);
+int metadata(FILE *fp);
+int fend(FILE *fp);
+FindResult *find(FILE *fp, char name[]);
+int find_name(FILE *fp, char objname[], int option);
+int move_block(FILE *fp, FILE *op, char objname[]);
+int import(FILE *fp, char fname[], char objname[]);
+int close(FILE *fp);
+
 
 #endif
