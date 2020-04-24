289b438ab8131c5eb5491c612ae2a84179e8c037
mark <you@example.com>
2020-04-25 02:14:56 +0300

    + deleteResult

--- project3/objdb.h
+++ project3/objdb.h
@@ -16,6 +16,7 @@ int open(FILE **fp, char dbname[]);
 int metadata(FILE *fp);
 int fend(FILE *fp);
 FindResult *find(FILE *fp, char name[]);
+void deleteResult(FindResult *result);
 int find_name(FILE *fp, char objname[], int option);
 int move_block(FILE *fp, FILE *op, char objname[]);
 int import(FILE *fp, char fname[], char objname[]);
