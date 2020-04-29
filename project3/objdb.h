4db17778cd3a09817092493052fb95ac2082985f
mark <you@example.com>
2020-04-29 21:51:05 +0300

    Fixed alignment

--- project3/objdb.h
+++ project3/objdb.h
@@ -1,25 +1,21 @@
-
-
 #ifndef _OBJDB_H
-#define _OBJDB_H
-#include <stdio.h>
+    #define _OBJDB_H
+    #include <stdio.h>
 
-typedef struct {
-    int num_results;
-    char *names_buffer;
-    int names_len;
-} FindResult;
+    typedef struct {
+        int num_results;
+        int names_len;
+        char *names_buffer;
+    } FindResult;
 
-int main(int argc, char *argv[]);
-void fexit(FILE *fp,const char func[],const int line);
-int open(FILE **fp, char dbname[]);
-int metadata(FILE *fp);
-int fend(FILE *fp);
-FindResult *find(FILE *fp, char name[], int called_by);
-void deleteResult(FindResult *result);
-int move_block(FILE *fp, FILE *op, char objname[]);
-int import(FILE *fp, char fname[], char objname[]);
-int close(FILE **fp);
-
-
+    int main(int argc, char *argv[]);
+    void fexit(FILE *fp,const char func[],const int line);
+    int open(FILE **fp, char dbname[]);
+    int metadata(FILE *fp);
+    int fend(FILE *fp);
+    FindResult *find(FILE *fp, char name[], int called_by);
+    void deleteResult(FindResult *result);
+    int move_block(FILE *fp, FILE *op, char objname[]);
+    int import(FILE *fp, char fname[], char objname[]);
+    int close(FILE **fp);
 #endif
