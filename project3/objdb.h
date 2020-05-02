55ec7103eccece9755160820b0503c6e68bf16d0
mark <you@example.com>
2020-05-03 00:42:27 +0300

    Finished export,move_in_db, move_512, move_from_db

--- project3/objdb.h
+++ project3/objdb.h
@@ -6,6 +6,8 @@
         int num_results;
         int names_len;
         char *names_buffer;
+        int object_position;
+        int object_size;
     } FindResult;
 
     int main(int argc, char *argv[]);
@@ -18,7 +20,7 @@
     FindResult *find(FILE **fp, char name[], int called_by);
     void deleteResult(FindResult *result, int called_by);
     int move_in_db(FILE *fp, FILE *op, char objname[]);
-    int move_from_db(FILE *fp, FILE *op, char fname[]);
+    int move_from_db(FILE *fp, FILE *op, char fname[], int object_position);
     int import(FILE **fp, char fname[], char objname[]);
     int export (FILE **fp, char objname[], char fname[]);
     int close(FILE **fp);
