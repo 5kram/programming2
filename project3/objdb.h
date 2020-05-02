233f55a18a4b64611b2e6075b68032b585284ee5
mark <you@example.com>
2020-05-02 19:58:07 +0300

    Double pointer to imp/exp/find etc

--- project3/objdb.h
+++ project3/objdb.h
@@ -11,15 +11,15 @@
     int main(int argc, char *argv[]);
     void read(void *ptr, int size_t_, int nmemb, FILE *stream);
     void write(void *ptr, int size_t_, int nmemb, FILE *stream);
-    void fexit(FILE *fp,const char func[],const int line);
+    void fexit(FILE **fp,const char func[],const int line);
     int open(FILE **fp, char dbname[]);
     int metadata(FILE *fp);
     int fend(FILE *fp);
-    FindResult *find(FILE *fp, char name[], int called_by);
+    FindResult *find(FILE **fp, char name[], int called_by);
     void deleteResult(FindResult *result, int called_by);
     int move_in_db(FILE *fp, FILE *op, char objname[]);
     int move_from_db(FILE *fp, FILE *op, char fname[]);
-    int import(FILE *fp, char fname[], char objname[]);
-    int export (FILE *fp, char objname[], char fname[]);
+    int import(FILE **fp, char fname[], char objname[]);
+    int export (FILE **fp, char objname[], char fname[]);
     int close(FILE **fp);
 #endif
